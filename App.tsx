6ab1ed74c1dfcd2c0ba1d2ea181d3ade21f13e3c
jihedr-ai <jihed.r@insignexpress.com>
2026-03-10 12:15:14 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1008,7 +1008,7 @@ const App: React.FC = () => {
       const data = {
         type: 'WETAG_ADD_TO_CART',
         payload: {
-          sku: 'CT-01',
+          sku: 'BADGE-NATIVE',
           qty: state.items.length,
           price: (totalPrice / state.items.length).toFixed(2),
           totalPrice: totalPrice.toFixed(2),
